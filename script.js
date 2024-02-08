282add8ce2d2e1b0e5bdc0ffe60c0b9b691fd92d
trinebra <157688079+trinebra@users.noreply.github.com>
2024-02-08 17:29:52 +0100

    updates

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var buttonOn = '';
+
+function TurnOfforOn() {
+    let lightOnorOff = Element.style.backgroundColor
+    if (lightOnorOfF == true) {
+        backgroundColor = 'yellow';
+    } else (lightOnorOfF == false)
+    { backgroundColor = ''; }
+    console.log()
+}
